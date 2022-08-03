44394af3df7babf17575eaa9081c362b1c27449b
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-08-03 17:09:46 -0400

    beginning of functionality

---
+++ main.js
@@ -0,0 +1,24 @@
+
+    fetch("http://localhost:8000/articles")
+    .then(response => response.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+        console.log('Success:', JSON.stringify(response));
+
+        const data = response;
+
+
+        const redShirt = document.querySelector("#redShirt")
+        redShirt.addEventListener("click", summonPants)
+
+        function summonPants () {
+            const redShirtMatch = document.querySelector("#redShirtMatch")
+            redShirtMatch.innerHTML = data.shirts[0].red[0].pants[0]
+
+            // redShirtMatch.innerHTML = response.shirts[0].red[0].pants.grey
+        }
+
+    
+    
+    
+    });
