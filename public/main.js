85e0d9e4241aa3217de6ec9ebf5bdc0a01439948
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-08-08 18:28:59 -0400

    more articles

--- public/main.js
+++ public/main.js
@@ -8,20 +8,23 @@
         const data = response;
 
         console.log(data)
-        console.log('data')
 
-        // const whiteShirt = document.querySelector("#whiteShirt")
-        // whiteShirt.addEventListener("click", showChoices)
+        const whiteShirt = document.querySelector("#whiteShirt")
+        whiteShirt.addEventListener("click", showChoices)
 
-        // function showChoices () {
-            // const jacket = document.querySelector("#jacket")
-            // const pants = document.querySelector("#pants")
-            // const shoes = document.querySelector("#shoes")
+        function showChoices () {
+            const jacket = document.querySelector("#jacket")
+            const pants = document.querySelector("#pants")
+            const shoes = document.querySelector("#shoes")
 
-            // jacket.innerHTML = data.articles.shirts[0].white[0].jackets[0]
-            // pants.innerHTML = data.shirts[0].white[0].pants[0]
-            // shoes.innerHTML = data.shirts[0].white[0].shoes[0]
+            jacket.innerHTML = data.shirts[0].white[0].jackets[0]
+            pants.innerHTML = data.shirts[0].white[0].pants[0]
+            shoes.innerHTML = data.shirts[0].white[0].shoes[0]
 
-        // }
+        }
     });
 
+// Three of each type of clothes, when press shirt, show an list of different shirts. Click one and that assigns your box as that color.
+// When you press pants, that shows what options are available
+
+//************************************************************************************************************************
