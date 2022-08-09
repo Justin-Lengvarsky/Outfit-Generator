e4636658bf95953dbf573fb2c9a590091580a0e8
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-08-09 19:26:12 -0400

    more article selection functionality

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 
-    fetch("http://localhost:7000/articles")
+    fetch("http://localhost:8000/articles")
     .then(response => response.json())
     .catch(error => console.error('Error:', error))
     .then(response => {
@@ -9,19 +9,70 @@
 
         console.log(data)
 
-        const whiteShirt = document.querySelector("#whiteShirt")
-        whiteShirt.addEventListener("click", showChoices)
 
-        function showChoices () {
-            const jacket = document.querySelector("#jacket")
-            const pants = document.querySelector("#pants")
-            const shoes = document.querySelector("#shoes")
+        const pickJacket = document.querySelector("#pickJacket")
+        const pickShirt = document.querySelector("#pickShirt")
+        const pickPants = document.querySelector("#pickPants")
+        const pickShoes = document.querySelector("#pickShoes")
+        const clothingOptions = document.querySelector("#clothingOptions")
 
-            jacket.innerHTML = data.shirts[0].white[0].jackets[0]
-            pants.innerHTML = data.shirts[0].white[0].pants[0]
-            shoes.innerHTML = data.shirts[0].white[0].shoes[0]
 
+        pickJacket.addEventListener("click", showJacketChoices)
+
+        function showJacketChoices () {
+
+            function removeAllChildNodes(parent) {
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                }
+            }
+            removeAllChildNodes(clothingOptions)
+
+            for (let i=0; i<data.jackets[0].colors.length; i++) {
+                var li = document.createElement("li")
+                li.appendChild(document.createTextNode(data.jackets[0].colors[i]))     
+                clothingOptions.appendChild(li)
+            }
         }
+
+
+
+        // const white = document.querySelector("#whiteShirt")
+        // const lightBlue = document.querySelector("#lightBlueShirt")
+        // const grey = document.querySelector("#greyShirt")
+
+        // white.addEventListener("click", showChoices)
+
+        // function showChoices () {
+        //     const jacket = document.querySelector("#jacket")
+        //     const pants = document.querySelector("#pants")
+        //     const shoes = document.querySelector("#shoes")
+
+        //     for (let i=0; i<data.shirts[0].white[0].jackets.length; i++) {
+        //         var li = document.createElement("li")
+        //         li.appendChild(document.createTextNode(data.shirts[0].white[0].jackets[i]))     
+        //         jacket.appendChild(li)
+        //     }
+
+        //     for (let i=0; i<data.shirts[0].white[0].pants.length; i++) {
+        //         var li = document.createElement("li")
+        //         li.appendChild(document.createTextNode(data.shirts[0].white[0].pants[i]))     
+        //         pants.appendChild(li)
+        //     }
+
+        //     for (let i=0; i<data.shirts[0].white[0].shoes.length; i++) {
+        //         var li = document.createElement("li")
+        //         li.appendChild(document.createTextNode(data.shirts[0].white[0].shoes[i]))     
+        //         shoes.appendChild(li)
+        //     }
+
+            // jacket.innerHTML = data.shirts[0].white[0].jackets[0]
+            // pants.innerHTML = data.shirts[0].white[0].pants[0]
+            // shoes.innerHTML = data.shirts[0].white[0].shoes[0]
+
+        // }
+
+
     });
 
 // Three of each type of clothes, when press shirt, show an list of different shirts. Click one and that assigns your box as that color.
