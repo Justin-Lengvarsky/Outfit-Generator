d4f122e76fefe6df6504528f664ddb4416281ffa
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-08-24 18:33:08 -0400

    started server connect

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 // json-server --watch db.json --port 8000
 
 fetch("http://localhost:8000/articles")
@@ -137,3 +136,76 @@ fetch("http://localhost:8000/articles")
         clearAllChildNodes(clothingOptions)
     }
 });
+
+
+
+
+// const deleteBtn = document.querySelectorAll('.del')
+// const todoItem = document.querySelectorAll('span.not')
+// const todoComplete = document.querySelectorAll('span.completed')
+
+// Array.from(deleteBtn).forEach((el)=>{
+//     el.addEventListener('click', deleteTodo)
+// })
+
+// Array.from(todoItem).forEach((el)=>{
+//     el.addEventListener('click', markComplete)
+// })
+
+// Array.from(todoComplete).forEach((el)=>{
+//     el.addEventListener('click', markIncomplete)
+// })
+
+// async function deleteTodo(){
+//     const todoId = this.parentNode.dataset.id
+//     try{
+//         const response = await fetch('todos/deleteTodo', {
+//             method: 'delete',
+//             headers: {'Content-type': 'application/json'},
+//             body: JSON.stringify({
+//                 'todoIdFromJSFile': todoId
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+
+// async function markComplete(){
+//     const todoId = this.parentNode.dataset.id
+//     try{
+//         const response = await fetch('todos/markComplete', {
+//             method: 'put',
+//             headers: {'Content-type': 'application/json'},
+//             body: JSON.stringify({
+//                 'todoIdFromJSFile': todoId
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+
+// async function markIncomplete(){
+//     const todoId = this.parentNode.dataset.id
+//     try{
+//         const response = await fetch('todos/markIncomplete', {
+//             method: 'put',
+//             headers: {'Content-type': 'application/json'},
+//             body: JSON.stringify({
+//                 'todoIdFromJSFile': todoId
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
--- server.js
+++ server.js
@@ -1,52 +1,46 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
-const PORT = 8000
-// const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient
-require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const todoRoutes = require('./routes/todos')
 
+require('dotenv').config({path: './config/.env'})
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'outfit-generator'
+// Passport config
+require('./config/passport')(passport)
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(express.static('public'))
-    app.use(bodyParser.json())
-    app.set('view engine', 'ejs')
-
-
-    app.get('/', (req, res) => {
-        db.collection('clothingArticles').find().toArray()
-          .then(results => {
-            res.render('index.ejs', {clothingArticles: results})
-            // console.log(results)
-            // console.log("yooo")
-
-          })
-          .catch(err => {
-            console.error(err)
-          })
-      })   
-
-      app.post('/addOutfit', (request, response) => {
-        db.collection('clothingArticles').insertOne({
-            title: "blah"
-        })
-        .then(result => {
-            console.log('Outfit Added')
-            response.redirect('/')
-        })
-        .catch(error => console.error(error))
-    })
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
