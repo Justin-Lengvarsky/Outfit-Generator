fb71f3ebc5c13a767e32104f63ddafd4f9e326b6
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-09-01 11:32:29 -0400

    began mobile responsiveness

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -184,3 +184,18 @@ async function deleteFavorite(){
         console.log(err)
     }
 }
+
+
+$(document).ready(function() {
+    // JQUERY NAV TOGGLE
+    $('#menu').bind('click',function(event){
+        $('#mainnav ul').slideToggle();
+    });
+
+    $(window).resize(function(){  
+        var w = $(window).width();  
+        if(w > 768) {  
+            $('#mainnav ul').removeAttr('style');  
+        }  
+    });
+    });
--- public/js/main.js
+++ public/js/main.js
@@ -124,3 +124,18 @@ function clearOptions () {
     }
     clearAllChildNodes(clothingOptions)
 }
+
+
+$(document).ready(function() {
+    // JQUERY NAV TOGGLE
+    $('#menu').bind('click',function(event){
+        $('#mainnav ul').slideToggle();
+    });
+
+    $(window).resize(function(){  
+        var w = $(window).width();  
+        if(w > 768) {  
+            $('#mainnav ul').removeAttr('style');  
+        }  
+    });
+    });
