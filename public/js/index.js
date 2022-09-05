ff352c0656359fab125bbc705cfc443a2eca048a
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-09-05 09:15:50 -0400

    added index page animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,52 +1,56 @@
 let jacketDiv = document.querySelector("#jacketDiv")
 let shirtDiv = document.querySelector("#shirtDiv")
+let pantsDiv = document.querySelector("#pantsDiv")
+let shoesDiv = document.querySelector("#shoesDiv")
+
 
 
 let jackets = {
     1: "navy",
-    2: "grey",
-    3: "black",
-    4: "burgundy"
+    2: "charcoal",
+    3: "burgundy",
+    4: "black"
 }
 
 let shirts = {
     1: "lightBlue",
-    2: "white",
-    3: "grey",
-    4: "white"
+    2: "pink",
+    3: "white",
+    4: "grey"
 }
 
 let pants = {
     1: "khaki",
-    2: "grey",
-    3: "black",
-    4: "burgundy"
+    2: "charcoal",
+    3: "burgundy",
+    4: "black"
 }
 
 let shoes = {
     1: "brown",
-    2: "burgundy",
-    3: "black",
+    2: "black",
+    3: "burgundy",
     4: "black"
 }
 
 
-let jacketI = 1;
-let shirtI = 1;
-let pantsI = 1;
-let shoesI = 1;
+let jacketI = 0;
+let shirtI = 0;
+let pantsI = 0;
+let shoesI = 0;
 
-setTimeout (function () {
 
-    let jackett = document.querySelector("#jackett")
+
+let jackett = document.querySelector("#jackett")
 
 
 function jacketLoop () {
-        setTimeout(function() {   
-            $(jacketDiv).animate({ opacity: 0 }, 0, function () {
+        setTimeout(function() {
+            $(jacketDiv).animate({ opacity: 0 }, 1000, function () {
+                jackett.src=`/images/jackets/${jackets[jacketI]}.png`
                 $(this).animate({ opacity: 1 }, 1000);
+
             });
-            jackett.src=`/images/jackets/${jackets[jacketI]}.png`
             jackett.style.opacity = "1"
             jacketI++;                    
             if (jacketI <= 4) {           
@@ -55,10 +59,10 @@ function jacketLoop () {
                 jacketI = 1;
                 jacketLoop(); 
             }                   
-        }, 2000)
+        }, 8000)
 }
 jacketLoop();
-}, 1000)
+
 
 setTimeout (function () {
 
@@ -66,10 +70,10 @@ setTimeout (function () {
 
     function shirtLoop () {
             setTimeout(function() {  
-                $(shirtDiv).animate({ opacity: 0 }, 0, function () {
+                $(shirtDiv).animate({ opacity: 0 }, 1000, function () {
+                    shirtt.src=`/images/shirts/${shirts[shirtI]}.png`
                     $(this).animate({ opacity: 1 }, 1000);
                 }); 
-                shirtt.src=`/images/shirts/${shirts[shirtI]}.png`
                 shirtI++;                    
                 if (shirtI <= 4) {           
                     shirtLoop();             
@@ -77,66 +81,51 @@ setTimeout (function () {
                     shirtI = 1;
                     shirtLoop(); 
                 }                   
-            }, 2000)
+            }, 8000)
     }
     shirtLoop();
 
 }, 2000)
 
-// setTimeout (function () {
+setTimeout (function () {
 
-//     function pantsLoop () {
-//         setTimeout(function() {   
-//             setTimeout(function() {   
-//                 indexImage.childNodes[5].src=`/images/pants/${pants[pantsI]}.png`
-//                 pantsI++;                    
-//                 if (pantsI <= 4) {           
-//                     pantsLoop();             
-//                 } else {
-//                     pantsI = 1;
-//                     pantsLoop(); 
-//                 }                   
-//             }, 4000)
-//         }, 1000)
-//     }
-//     pantsLoop();
+    function pantsLoop () {
+            setTimeout(function() {   
+                $(pantsDiv).animate({ opacity: 0 }, 1000, function () {
+                    pantss.src=`/images/pants/${pants[pantsI]}.png`
+                    $(this).animate({ opacity: 1 }, 1000);
+                }); 
+                pantsI++;                    
+                if (pantsI <= 4) {           
+                    pantsLoop();             
+                } else {
+                    pantsI = 1;
+                    pantsLoop(); 
+                }                   
+            }, 8000)
+    }
+    pantsLoop();
 
-// }, 3000)
+}, 4000)
 
-// setTimeout (function () {
+setTimeout (function () {
 
-//     function shoesLoop () {
-//         setTimeout(function() {   
-//             setTimeout(function() {   
-//                 indexImage.childNodes[7].src=`/images/shoes/${shoes[shoesI]}.png`
-//                 shoesI++;                    
-//                 if (shoesI <= 4) {           
-//                     shoesLoop();             
-//                 } else {
-//                     shoesI = 1;
-//                     shoesLoop(); 
-//                 }                   
-//             }, 4000)
-//         }, 1000)
-//     }
-//     shoesLoop();
+    function shoesLoop () {
+            setTimeout(function() {   
+                $(shoesDiv).animate({ opacity: 0 }, 1000, function () {
+                    shoess.src=`/images/shoes/${shoes[shoesI]}.png`
+                    $(this).animate({ opacity: 1 }, 1000);
+                }); 
+                shoesI++;                    
+                if (shoesI <= 4) {           
+                    shoesLoop();             
+                } else {
+                    shoesI = 1;
+                    shoesLoop(); 
+                }                   
+            }, 8000)
+    }
+    
+    shoesLoop();
 
-// }, 4000)
-
-
-
-
-
-    // setTimeout(function() {   
-    //     setTimeout(function() {   
-    //         indexImage.childNodes[3].src=`/images/shirts/${shirts[shirtI]}.png`
-    //         shirtI++;                    
-    //         if (shirtI <= 4) {           
-    //             jacketLoop();             
-    //         } else {
-    //             shirtI = 1;
-    //             jacketLoop(); 
-    //         }                   
-    //         }, 1000)
-    //     }, 1000)
-    //     window.setInterval(newQuoteFunc, 5000)
+}, 6000)
